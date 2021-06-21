3ebec2b8a47f61d63f5f91271795b398c2b29908
nekonekokun <32277391+derekli19951103@users.noreply.github.com>
2021-06-21 16:10:22 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 require("../styles/globals.less");
 import type { AppProps } from "next/app";
+import React from "react";
 
+React.useLayoutEffect = React.useEffect;
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
